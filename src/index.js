0f1dac2c1d6810b0406d3a10dc20494596a4e0f8
Kyle Hultgren <kylehultgren99@yahoo.com>
2022-02-13 15:42:59 -0800

    Added Home, ErrorPage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MyApp from './MyApp';
 import './index.css';
+import Home from './Home';
 
-ReactDOM.render(<MyApp />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
