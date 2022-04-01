be2927ab51b69f7c1e0f9a60aed5f040188579e3
bbenzie42 <55561432+bbenzie42@users.noreply.github.com>
2022-04-01 15:42:52 -0700

    added base functionality

--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useState} from "react";
 import { useNavigate } from 'react-router-dom'
 import './CSS/signup.css';
 
-function SignUpPage(){
+function SignUpPage(props){
     let navigate = useNavigate(); 
 
-    return(
-        <div>
-            <h1>Sign Up</h1>
+    const [user, setUser] = useState({
+        username: "",
+        email: "",
+        pwd: "",
+      });
 
-            
-        </div>
+      function handleChange(event) {
+        const { name, value } = event.target;
+        if (name === "email") setUser({ ...user, email: value });
+        else if (name === "username") setUser({ ...user, username: value });
+        else if (name === "pwd") setUser({ ...user, pwd: value });
+      }
+
+      function submitForm() {
+        props.handleSubmit(user);
+        setUser({username: '', email: '', pwd: ''});
+        //TODO: go to user page on successful registration
+    }
+
+    return(
+        <form>
+        <label htmlFor="Username">Username</label>
+        <input
+            type="text"
+            name="username"
+            id="username"
+            value={user.username}
+            onChange={handleChange} />
+        <label htmlFor="Email">Bio</label>
+        <input
+            type="text"
+            name="email"
+            id="email"
+            value={user.email}
+            onChange={handleChange} />
+        <label htmlFor="pwd">Password</label>
+        <input
+            type="text"
+            name="pwd"
+            id="pwd"
+            value={user.pwd}
+            onChange={handleChange} />
+        <br></br>
+        <input name = "master-button" type="button" value="Submit All" onClick={submitForm} />
+        </form>      
     );
 }
 
