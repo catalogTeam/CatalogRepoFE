373a0f25e2a95eed56e7350e9e3ac6bdb8d26c1b
colewarner24 <74276573+colewarner24@users.noreply.github.com>
2022-06-01 15:22:04 -0700

    log

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe("login test", () => {
+  it("put username and password in feilds", () => {
+    cy.visit("http://localhost:3000/signup");
+
+    cy.get("[id^=li_username]")
+      .type("logintest")
+      .should("have.value", "logintest");
+
+    cy.get("[id^=li_password]").type("test").should("have.value", "test");
+  });
+
+  it("login to page", () => {
+    cy.get("button[type=submitLogin]").click();
+
+    cy.url().should("include", "/profile/logintest");
+  });
+});
